e32b9a654ba2631a564bfbc4ba8493f3aeff15e2
diegomrno <d@infinite.si>
2026-08-01 17:38:25 +0200

    feat: Create tarball with checksum file on build on Linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 rm -rf ../dist
+rm -rf ../prod
+
 mkdir -p ../dist
+mkdir -p ../prod
 
 MODULE_JSON_PATH="../plugin.json"
 NAME=$(jq -r .name $MODULE_JSON_PATH)
@@ -10,3 +13,12 @@ FOLDER_NAME="$NAME-$VERSION"
 mkdir -p "../dist/$FOLDER_NAME"
 cp -r ../src ../dist/$FOLDER_NAME/
 cp ../plugin.json ../dist/$FOLDER_NAME/
+
+
+TAR_NAME="../prod/$FOLDER_NAME.tar.gz"
+tar -czf "$TAR_NAME" -C ../dist "$FOLDER_NAME"
+
+echo "Archive created : $TAR_NAME"
+
+CHECKSUM_NAME="../prod/$FOLDER_NAME.tar.gz.sha256"
+sha256sum "$TAR_NAME" | awk '{print $1}' > "$CHECKSUM_NAME"
