0b14731c3c810601fe717cd8a5ae112e3420a9cf
diegomrno <d@infinite.si>
2026-08-03 17:30:09 +0200

    feat: Implementing Vortex plugin windows

---
+++ src/interfaces/test.lua
@@ -0,0 +1,5 @@
+
+Cherry.DrawRect(100, 100, 500, 500, "#0000FFFF")
+if Cherry.IsMouseClickedOnCurrentPos(100, 100, 500, 500, 0) then 
+    Vortex.Log("Clicked from file !!!")
+end
--- src/main.lua
+++ src/main.lua
@@ -3,3 +3,13 @@ Vortex.Log("Hello World !")
 Vortex.AddOutputEvent("save_all", function(args)
     Vortex.Log("Save all event triggered !")
 end, "none")
+
+Vortex.Window("Hello World", function()
+    Cherry.DrawRect(100, 100, 500, 500, "#FF0000FF")
+    if Cherry.IsMouseClickedOnCurrentPos(100, 100, 500, 500, 0) then 
+        Vortex.Log("Clicked !!!")
+    end
+end, "none")
+
+
+Vortex.Window("Hello World2", "src/interfaces/test.lua", "none")
